7c9cd50290bdbd6d9d5085b516e055410acf32e3
NiladriModak <niladrimodakk2001@gmail.com>
2024-07-22 00:36:01 +0530

    resolved reload error

--- fronend/vite.config.js
+++ fronend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
 export default defineConfig({
-  plugins: [react()],
+  envPrefix: "REACT_APP_",
+  plugins: [react(), envCompatible()],
 });
